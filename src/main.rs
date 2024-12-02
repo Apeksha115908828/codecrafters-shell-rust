534e78002730bc8fc0f0aedd153b091130ca0b28
Apeksha Vinayak Bodade <apeksha@7yb-vf.local>
2024-12-02 17:34:03 -0500

    Implemented type for paths

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ fn main() {
 
     // Wait for user input
     let stdin = io::stdin();
+    let path_var = std::env::var("PATH").unwrap();
+    let split_paths :Vec<&str> = path_var.split(":").collect();
     // let stdout = io::stdout();
     let mut input = String::new();
     let builtins = vec!["echo", "type", "exit"];
@@ -26,7 +28,23 @@ fn main() {
             if builtins.contains(&args[1]) {
                 println!("{} is a shell builtin", args[1]);
             } else {
-                println!("{}: not found", args[1]);
+                let mut found = false;
+                for i in 0..split_paths.len() {
+                    // let path = format!("{}/{}", path, args[1]);
+                    if split_paths[i].contains(args[1]) {
+                        println!("{} is {}", args[1], split_paths[i]);
+                        found = true;
+                        break;
+                    }
+                    // if std::path::Path::new(&path).exists() {
+                    //     println!("{} is {}", args[1], path);
+                    //     found = true;
+                    //     break;
+                    // }
+                }
+                if !found {
+                    println!("{}: not found", args[1]);
+                }
             }
         } else {
             println!("{}: command not found", input.trim());
