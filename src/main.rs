13d6f4445be74a3c145899d3c8763ba0d7bc2a65
Apeksha Vinayak Bodade <apeksha@7yb-vf.local>
2024-12-02 02:49:19 -0500

    Implemented type command for builtins

--- src/main.rs
+++ src/main.rs
@@ -10,16 +10,24 @@ fn main() {
     let stdin = io::stdin();
     // let stdout = io::stdout();
     let mut input = String::new();
+    let builtins = vec!["echo", "type", "exit"];
     // implementing a REPL (Read-Eval-Print Loop).
     loop {
         stdin.read_line(&mut input).unwrap();
-        if input.trim() == "exit 0" {
+        if input.contains("exit 0") {
             return;
         }
-        if input.trim().contains("echo") {
+        if input.trim().starts_with("echo") {
             let args :Vec<&str> = input.split_whitespace().collect();
             println!("{}", args[1..].join(" "));
             // println!("{}", args[1]);
+        } else if input.trim().starts_with("type") {
+            let args :Vec<&str> = input.split_whitespace().collect();
+            if builtins.contains(&args[1]) {
+                println!("{} is a shell builtin", args[1]);
+            } else {
+                println!("{}: not found", args[1]);
+            }
         } else {
             println!("{}: command not found", input.trim());
         }
