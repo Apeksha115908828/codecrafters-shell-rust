ec038afadeb72d6a834f875af9d00482ef4e179c
Apeksha Vinayak Bodade <apeksha@7yb-vf.local>
2024-12-02 01:46:21 -0500

    Added support for invalid_command

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,20 @@ fn main() {
 
     // Wait for user input
     let stdin = io::stdin();
+    let mut stdout = io::stdout();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
+    // if input.trim() == "invalid_command" {
+    // stdout.write_all(input.as_bytes()).unwrap();
+    // stdout.write_all(b": command not found\n").unwrap();
+    println!("{}: command not found", input.trim());
+    // stdout.write_all(input + b": command not found\n").unwrap();
+    // stdout.unlock();
+    // return println!("invalid_command: command not found");
+    // } else {
+    //     stdout.write_all(input.as_bytes()).unwrap();
+    //     stdout.write_all(b"\n").unwrap();
+    // stdout.unlock();
+    // return println!("{}", input);
+    // }
 }
