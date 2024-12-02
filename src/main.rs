9d47ab3e4020599dc013143d6a7b2b56a2f5c4b0
Apeksha Vinayak Bodade <apeksha@7yb-vf.local>
2024-12-02 02:08:57 -0500

    Implement REPL

--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,13 @@ fn main() {
 
     // Wait for user input
     let stdin = io::stdin();
-    let mut stdout = io::stdout();
+    // let stdout = io::stdout();
     let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
-    // if input.trim() == "invalid_command" {
-    // stdout.write_all(input.as_bytes()).unwrap();
-    // stdout.write_all(b": command not found\n").unwrap();
-    println!("{}: command not found", input.trim());
-    // stdout.write_all(input + b": command not found\n").unwrap();
-    // stdout.unlock();
-    // return println!("invalid_command: command not found");
-    // } else {
-    //     stdout.write_all(input.as_bytes()).unwrap();
-    //     stdout.write_all(b"\n").unwrap();
-    // stdout.unlock();
-    // return println!("{}", input);
-    // }
+    loop {
+        stdin.read_line(&mut input).unwrap();
+        println!("{}: command not found", input.trim());
+        input.clear();
+        print!("$ ");
+        io::stdout().flush().unwrap();
+    }
 }
