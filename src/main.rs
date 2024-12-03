7c56c37985eb3b447c98ce6c27563ac673ffe979
Apeksha Vinayak Bodade <apeksha@7yb-vf.local>
2024-12-02 20:57:48 -0500

    >

--- src/main.rs
+++ src/main.rs
@@ -28,23 +28,31 @@ fn main() {
             if builtins.contains(&args[1]) {
                 println!("{} is a shell builtin", args[1]);
             } else {
-                let mut found = false;
-                for i in 0..split_paths.len() {
+                // let mut found = false;
+                // for i in 0..split_paths.len() {
                     // let path = format!("{}/{}", path, args[1]);
-                    if split_paths[i].contains(args[1]) {
-                        println!("{} is {}", args[1], split_paths[i]);
-                        found = true;
-                        break;
-                    }
+                    // if split_paths[i].contains(args[1]) {
+                    //     println!("{} is {}", args[1], split_paths[i]);
+                    //     found = true;
+                    //     break;
+                    // }
                     // if std::path::Path::new(&path).exists() {
                     //     println!("{} is {}", args[1], path);
                     //     found = true;
                     //     break;
                     // }
-                }
-                if !found {
+                // }
+                let split = &mut path_var.split(':');
+                if let Some(path) =
+                    split.find(|path| std::fs::metadata(format!("{}/{}", path, args[1])).is_ok())
+                {
+                    println!("{} is {}/{}", args[1], path, args[1]);
+                } else {
                     println!("{}: not found", args[1]);
                 }
+                // if !found {
+                //     println!("{}: not found", args[1]);
+                // }
             }
         } else if input.trim().starts_with("pwd") {
             let current_dir = std::env::current_dir().unwrap();
